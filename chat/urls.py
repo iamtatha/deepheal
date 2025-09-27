cb4757bd056fe124337d50702036b981f9b3e866
iamtatha <tathagata2403@gmail.com>
2025-09-27 11:51:57 +0530

    working copy of realtime audio video storing processing and transcription

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import chat_page, chat_api, logs_view
+from .views import chat_page, chat_api, logs_view, process_audio
 
 urlpatterns = [
     path("", chat_page, name="chat_page"),
-    path("api/chat/", chat_api, name="chat_api"),
+    path("api/", chat_api, name="chat_api"),
     path("logs/", logs_view, name="logs"),
+    path("process_audio/", process_audio, name="process_audio"),
 ]
